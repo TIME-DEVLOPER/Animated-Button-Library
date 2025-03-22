d8fb293b4539e3a61fe8c3ae2129d3216b795869
Samay raj <samayraj099@gmail.com>
2025-03-22 20:56:50 +0530

    Update button-effects.js

--- js/button-effects.js
+++ js/button-effects.js
@@ -1,17 +1,4 @@
 
-//  Glow Effect JS 
-document.addEventListener("DOMContentLoaded", function () {
-    let buttons = document.querySelectorAll(".btn-glow");
-    buttons.forEach(button => {
-        button.addEventListener("mouseover", () => {
-            button.style.boxShadow = "0 0 30px rgba(76, 175, 80, 1)";
-        });
-        button.addEventListener("mouseout", () => {
-            button.style.boxShadow = "0 0 20px rgba(76, 175, 80, 0.8)";
-        });
-    });
-});
-
 // 🔥 Ripple Effect JS 🔥
 document.addEventListener("DOMContentLoaded", function () {
     document.querySelectorAll(".btn-ripple").forEach(button => {
