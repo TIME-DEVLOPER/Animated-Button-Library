dd39957d8f108628c7a14cc9a8835483915fc780
Samay raj <samayraj099@gmail.com>
2025-03-22 20:39:53 +0530

    Update button-effects.js

--- js/button-effects.js
+++ js/button-effects.js
@@ -1,1 +1,13 @@
-// Button Effects JS File
+
+//  Glow Effect JS 
+document.addEventListener("DOMContentLoaded", function () {
+    let buttons = document.querySelectorAll(".btn-glow");
+    buttons.forEach(button => {
+        button.addEventListener("mouseover", () => {
+            button.style.boxShadow = "0 0 30px rgba(76, 175, 80, 1)";
+        });
+        button.addEventListener("mouseout", () => {
+            button.style.boxShadow = "0 0 20px rgba(76, 175, 80, 0.8)";
+        });
+    });
+});
